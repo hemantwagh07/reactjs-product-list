d1700b5f146077d63013a583f9e75c782fa33d04
Hemant Wagh <hemantwagh07@gmail.com>
2020-09-24 21:09:06 +0530

    Updates

---
+++ precache-manifest.2f76dfb684e9154e73372a094cc4688d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36085c2596231045f1abf94294ceae9",
+    "url": "/reactjs-product-list/index.html"
+  },
+  {
+    "revision": "88a18cbef73b93b22969",
+    "url": "/reactjs-product-list/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5c7a6a3dcf199b45c74c",
+    "url": "/reactjs-product-list/static/css/main.85ee5e94.chunk.css"
+  },
+  {
+    "revision": "88a18cbef73b93b22969",
+    "url": "/reactjs-product-list/static/js/2.2ac7c73f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reactjs-product-list/static/js/2.2ac7c73f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7a6a3dcf199b45c74c",
+    "url": "/reactjs-product-list/static/js/main.ec48eacd.chunk.js"
+  },
+  {
+    "revision": "cd6e94476189e3900afb",
+    "url": "/reactjs-product-list/static/js/runtime-main.71e8b62f.js"
+  }
+]);
